74919f18fdcb4e39dd606290dd0280a18f912796
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-20 16:47:04 -0500

    up to iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,33 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router()
+const Celebrity = require("./../models/Celebrity.model")
+const Movie = require("./../models/Movie.model")
+const mongoose = require("mongoose")
 
-// all your routes here
+router.get("/movies/create", (req, res) => {
+  Celebrity.find()
+    .then((celebritiesFound) =>
+      res.render("movies/new-movie", {
+        celebrities: celebritiesFound,
+      })
+    )
+    .catch((e) => console.log(e))
+})
+
+router.post("/movies/create", (req, res) => {
+  const { title, genre, plot, castIds } = req.body
+  const cast = []
+  for (let i = 0; i < castIds.length; i++) {
+    cast.push(mongoose.Types.ObjectId(castIds[i]))
+  }
+
+  console.log(cast)
+
+  Movie.create({ title, genre, plot, cast })
+    .then(() => {
+      res.redirect("/movies")
+    })
+    .catch((e) => console.log(e))
+})
 
 module.exports = router
