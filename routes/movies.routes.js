a7ae7ad7554762d576caf8252e34a7c64f80dc42
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-21 10:26:01 -0500

    bonus

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -63,19 +63,43 @@ router.post("/movies/:id/delete", (req, res) => {
     .catch((e) => console.log(e))
 })
 
+/* BEFORE BONUS, KEEPING IN CASE IT BREAKS */
+// router.get("/movies/:id/edit", (req, res) => {
+//   Movie.findById(req.params.id)
+//     .populate("cast")
+//     .then((movie) => {
+//       Celebrity.find().then((allCelebrities) => {
+//         // console.log("MovieFound ", movie)
+//         // console.log("Celebrities in movie", celebrities)
+//         // console.log("All Celebrities", allCelebrities)
+//         res.render("movies/edit-movie", {
+//           movie,
+//           allCelebrities,
+//         })
+//       })
+//     })
+//     .catch((e) => console.log(e))
+// })
+
 router.get("/movies/:id/edit", (req, res) => {
   Movie.findById(req.params.id)
+    .populate("cast")
     .then((movie) => {
-      Celebrity.find({ _id: { $in: movie.cast } }).then((celebrities) => {
-        Celebrity.find().then((allCelebrities) => {
-          console.log("MovieFound ", movie)
-          console.log("Celebrities in movie", celebrities)
-          console.log("All Celebrities", allCelebrities)
-          res.render("movies/edit-movie", {
-            movie,
-            celebrities,
-            allCelebrities,
-          })
+      Celebrity.find().then((allCelebrities) => {
+        for (let i = 0; i < allCelebrities.length; i++) {
+          if (
+            //if we find a celebrity that's in the movie,
+            //add a "selected" parameter
+            movie.cast.some(
+              (e) => e._id.toString() === allCelebrities[i]._id.toString()
+            )
+          ) {
+            allCelebrities[i].selected = true
+          }
+        }
+        res.render("movies/edit-movie", {
+          movie,
+          allCelebrities,
         })
       })
     })
@@ -84,8 +108,19 @@ router.get("/movies/:id/edit", (req, res) => {
 
 router.post("/movies/:id/edit", (req, res) => {
   const { title, genre, plot, castIds } = req.body
-  Movie.findByIdAndUpdate(req.params.id, { title, genre, plot })
-    .then(() => {
+
+  const cast = []
+
+  for (let i = 0; i < castIds.length; i++) {
+    cast.push(mongoose.Types.ObjectId(castIds[i]))
+  }
+
+  Movie.findByIdAndUpdate(
+    req.params.id,
+    { title, genre, plot, cast },
+    { new: true }
+  )
+    .then((newMovie) => {
       res.redirect("/movies")
     })
     .catch((e) => console.log(e))
